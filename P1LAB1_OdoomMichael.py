68aaa0a4ed41da252c92ab684c90ea8b6a9fee3b
odoomm7735 <odoomm7735@student.faytechcc.edu>
2024-06-19 12:30:41 -0400

    Add files via upload

---
+++ P1LAB1_OdoomMichael.py
@@ -0,0 +1,14 @@
+
+
+# Your Name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Date
+date = input("Today's date: ")
+
+# Assignment Name
+print("Hello " + first_name + " " + last_name + "! Welcome to CTI-110")
+
+# A brief description of the project
+print ("Course Description: CTI-110: How Gurus become Durus")
